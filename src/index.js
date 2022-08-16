7dbed2ff2aa7723d377df93205dfbab9b99de144
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-08-16 23:30:28 +0200

    add files implement responsiveness, hover, delay, generic resize function,  mobile click functions, tab navigation

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import './style.css'
+
+// Prevent showing animation on window resize
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
+
+// Menu toogle on mobile
+const navToggle = document.querySelector('.nav-toggle');
+const menuToggle = document.querySelector('.menu-toggle');
+
+navToggle.addEventListener('click', function (e) {
+  this.classList.toggle('open');
+  menuToggle.classList.toggle('active');
+  e.stopPropagation();
+});
+
+
+
+// Dropdown toogle on mobile
+const dropdown = document.querySelectorAll('.dropdown a');
+
+Array.from(dropdown)
+  .map(element => {
+    element
+      .addEventListener('click', function (e) {
+        this.nextElementSibling.classList.toggle('show');
+        this.parentNode.classList.toggle('active');
+        e.stopPropagation();
+      })
+
+  });
+
+
+// Second level dropdown toggle on mobile
+const deepDropdown = document.querySelectorAll('.dropdown second-level a');
+
+Array.from(deepDropdown)
+  .map(element => {
+    element
+      .addEventListener('click', function (e) {
+        this.nextElementSibling.classList.toggle('show');
+        this.parentNode.classList.toggle('active');
+        e.stopPropagation();
+      })
+  });
